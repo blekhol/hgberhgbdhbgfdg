2261b2f0a7dc97c1e01486fe38bf18e0bab697a5
asd <asd@gmail.com>
2026-04-15 10:01:55 +0200

    ennyit mára

--- WpfApp5/MainWindow.xaml.cs
+++ WpfApp5/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -17,6 +18,23 @@ namespace WpfApp5
         public MainWindow()
         {
             InitializeComponent();
-        }
+
+            Mozgatas(0);
+            Mozgatas(1);
+            Mozgatas(2);
+		}
+
+        void Mozgatas(int oszlop)
+        {
+            int[] oszlopMarginok = { 25, 285, 545 };
+
+            Rectangle rect = new Rectangle();
+            rect.Height = 150;
+            rect.Width = 210;
+            rect.Stroke = Brushes.Black;
+            rect.Fill = new SolidColorBrush(Color.FromRgb(222, 222, 222));
+            rect.HorizontalAlignment = HorizontalAlignment.Left;
+            rect.Margin = new Thickness(oszlopMarginok[oszlop], -400, 0, 0);
+		}
     }
 }
