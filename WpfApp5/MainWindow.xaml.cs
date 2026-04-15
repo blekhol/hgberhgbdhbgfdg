46ddc0e25597cffff7f33930bfb3fdbb123bd271
asd <asd@gmail.com>
2026-04-15 09:22:38 +0200

    asd

--- WpfApp5/MainWindow.xaml.cs
+++ WpfApp5/MainWindow.xaml.cs
@@ -11,9 +11,7 @@ using System.Windows.Shapes;
 
 namespace WpfApp5
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
